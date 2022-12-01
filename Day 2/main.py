11b9b09c6314b7fdca574f33fcd6197f8d2f37ee
Dan Bae <danbae@Dans-MacBook-Air.local>
2022-12-01 13:58:43 -0600

    edit day 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -2,7 +2,7 @@ print("Welcome to the tip calculator.")
 total_bill = float(input("What was the total bill? $"))
 tip_percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100 + 1
 people_split = int(input("How many people to split the bill? "))
-payment = round(total_bill * tip_percentage / people_split, 2)
+payment = "{:.2f}".format(total_bill * tip_percentage / people_split)
 message = f"Each person should pay: ${payment}"
 
 print(message)
